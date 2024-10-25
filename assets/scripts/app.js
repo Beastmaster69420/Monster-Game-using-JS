beab30f647a8daad871a1a6e2c2cd2c5c32c5fa6
Beastmaster69420 <rammhrzn0000@gmail.com>
2024-10-25 12:15:04 +0545

    attack update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
+const monsterAttackValue = 11;
 
-let chosenMaxLife = 100;
+let chosenMaxLife = 5;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(monsterAttackValue);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth<=0 && currentPlayerHealth>0){
+    alert("Winner");
+  }else if (currentPlayerHealth<=0 && currentMonsterHealth>0){
+    alert("Defeated");
+  }else if (currentPlayerHealth<=0 && currentPlayerHealth<=0){
+    alert("Stalemate!");
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
