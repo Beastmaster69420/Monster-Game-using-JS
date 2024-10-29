7739220b903018f98742f3a4ef0cbfa3cddb9b80
Beastmaster69420 <rammhrzn0000@gmail.com>
2024-10-29 16:45:52 +0545

    heal function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,15 @@
 const ATTACK_VALUE = 10;
+const BIG_ATK_VALUE = 19;
 const monsterAttackValue = 11;
+const HEALVAL = 5;
 
-let chosenMaxLife = 5;
+let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
+function endRound(){
   const playerDamage = dealPlayerDamage(monsterAttackValue);
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth<=0 && currentPlayerHealth>0){
@@ -21,4 +21,41 @@ function attackHandler() {
   }
 }
 
+function ATTACK_MONSTER(mode){
+  let maxDamage;
+  if (mode === 'ATTACK'){
+    maxDamage = ATTACK_VALUE;
+  }else if (mode === 'STRONG_ATTACK'){
+    maxDamage= BIG_ATK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
+}
+function attackHandler() {
+  ATTACK_MONSTER('ATTACK');
+}
+
+function strongAttackBtnClick(){
+  ATTACK_MONSTER('STRONG_ATTACK');
+}
+
+
+
+function onClickhealPlayer(){
+  let healValue;
+  if (currentPlayerHealth>=chosenMaxLife-HEALVAL) {
+    alert('Exceeds max life');
+    healValue = chosenMaxLife-currentPlayerHealth;
+  } else{
+    healValue = HEALVAL;
+  }
+
+  increasePlayerHealth(healValue);
+  currentMonsterHealth += healValue;
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click',strongAttackBtnClick);
+healBtn.addEventListener('click', onClickhealPlayer);
